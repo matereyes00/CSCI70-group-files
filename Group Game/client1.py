94b606ed454b942c1fe210f3711f96a81387e8ce
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2022-10-26 19:50:40 +0800

    Added rock paper scissors game on client side. Moves from server get sent to client. Logic errors: restarting the game, inputting new answers when restarting and cant exit game.

--- Group Game/client1.py
+++ Group Game/client1.py
@@ -3,6 +3,79 @@ from colorama import init
 from colorama import Fore, Back, Style
 init()
 
+def start_rps(p1Move, p2Move):
+    moves = ["scissors", "paper", "rock"]
+    p1_name_answers = []
+    p2_name_answers = []
+    wins = []
+    loses = []
+    while True:
+        p1_name_answers.append(p1Move)
+        p2_name_answers.append(p2Move)
+
+        if p1_name_answers[-1] == p2_name_answers[-1]: # TIE
+            print("Tie!")
+
+        elif p1_name_answers[-1] != p2_name_answers[-1]: 
+            # USER 1 == SCISSORS
+            if p1_name_answers[-1] == moves[0]: # scissors beat paper, scissors lose rock
+                if p2_name_answers[-1] == moves[1]:
+                    print(f"{p1_name} wins. Scissors beats paper")
+                    wins.append(p1_name)
+                    loses.append(p2_name)
+
+                elif p2_name_answers[-1] == moves[2]:
+                    print(f"{p2_name} wins. Rock beats scissors")
+                    wins.append(p2_name)
+                    loses.append(p1_name)
+                else:
+                    print("invalid move")
+            
+            # USER 1 == PAPER
+            elif p1_name_answers[-1] == moves[1]: # paper beats rock, paper lose scissors
+                if p2_name_answers[-1] == moves[0]:
+                    print(f"{p2_name} wins. Scissors beats paper")
+                    wins.append(p2_name)
+                    loses.append(p1_name)
+                
+                elif p2_name_answers[-1] == moves[2]:
+                    print(f"{p1_name} wins. Paper beats rock")
+                    wins.append(p2_name)
+                    loses.append(p1_name)
+
+                else:
+                    print("invalid move")
+            
+            # p1_name == ROCK
+            elif p1_name_answers[-1] == moves[2]: # rock beats scissors, rock lose paper
+                if p2_name_answers[-1] == moves[0]:
+                    print(f"{p1_name} wins. Rock beats Scissors")
+                    wins.append(p1_name)
+                    loses.append(p2_name)
+                
+                elif p2_name_answers[-1] == moves[1]:
+                    print(f"{p2_name} wins. Paper beats rock")
+                    wins.append(p2_name)
+                    loses.append(p1_name)
+
+                else:
+                    print("invalid move")
+            
+            # not valid option
+            elif p1_name_answers[-1] not in moves:
+                print("Sorry, not a valid move.")
+                p1Move = input("Try again: ")
+
+            # not valid option
+            elif p2_name_answers[-1] not in moves:
+                print("Sorry, not a valid move.")
+                p2Move = input("Try again: ")
+
+
+        play_again = input("Play again? (y/n): ")
+        if play_again.lower() != "y":
+            break
+
 
 def client_program():
     host = socket.gethostname()  # as both code is running on same pc
@@ -13,20 +86,24 @@ def client_program():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # instantiate
     client_socket.connect((host, port))  # connect to the server
 
-    P2_name = input("Please input your name: ")
-    client_socket.send(P2_name.encode(FORMAT))
+    global p2_name, p1_name, p2_move, p1_move
+
+    p2_name = input("Please input your name: ")
+    client_socket.send(p2_name.encode(FORMAT))
     p1_name = client_socket.recv(HEADER).decode(FORMAT)
     print(f"You are now chatting with {p1_name}")
-    message = input(" >>> ")  # take input
+    p2_move = input(" >>> ")  # take input of p2move
 
-    while message.lower().strip() != 'exit()':
-        client_socket.send(message.encode(FORMAT))  # send message
-        data = client_socket.recv(HEADER).decode(FORMAT)  # receive response
+    while p2_move.lower().strip() != 'exit()':
+        client_socket.send(p2_move.encode(FORMAT))  # send message
+        p1_move = client_socket.recv(HEADER).decode(FORMAT)  # receive response
 
-        print(Fore.YELLOW + f'[{P2_name} - {socket.gethostbyname(host)}]')
-        print(Fore.CYAN, f"Received a message from {p1_name}: {data}")  # show in terminal
+        print(Fore.YELLOW + f'[{p2_name} - {socket.gethostbyname(host)}]')
+        print(Fore.CYAN, f"{p1_name} did {p1_move}")  # show in terminal
 
-        message = input( " >>> ")  # again take input
+        start_rps(p1_move, p2_move)
+
+        # p2_move = input( " >>> ")  # again take input
 
     client_socket.close()  # close the connection
 
--- Group Game/server1.py
+++ Group Game/server1.py
@@ -4,76 +4,6 @@ from colorama import Fore, Back, Style
 import inspect
 init()
 
-
-def RPS_game(user1, user2):
-    user1 = input("Enter P1 name: ")
-    user2 = input("Enter P2 name: ")
-
-    moves = ["scissors", "paper", "rock"]
-    user1_answers = []
-    user2_answers = []
-
-    wins = []
-    loses = []
-
-    while True:
-        user1_move = input("Enter P1 move:")
-        user2_move = input("Enter P2 move:")
-        user1_answers.append(user1_move)
-        user2_answers.append(user2_move)
-
-        if user1_answers[-1] == user2_answers[-1]: # TIE
-            print("Tie!")
-
-        elif user1 != user2: 
-            # USER 1 == SCISSORS
-            if user1_answers[-1] == moves[0]: # scissors beat paper, scissors lose rock
-                if user2_answers[-1] == moves[1]:
-                    print(f"{user1} wins. Scissors beats paper")
-                    wins.append(user1)
-                    loses.append(user2)
-
-                elif user2_answers[-1] == moves[2]:
-                    print(f"{user2} wins. Rock beats scissors")
-                    wins.append(user2)
-                    loses.append(user1)
-                else:
-                    print("invalid move")
-            
-            # USER 1 == PAPER
-            elif user1_answers[-1] == moves[1]: # paper beats rock, paper lose scissors
-                if user2_answers[-1] == moves[0]:
-                    print(f"{user2} wins. Scissors beats paper")
-                    wins.append(user2)
-                    loses.append(user1)
-                
-                elif user2_answers[-1] == moves[2]:
-                    print(f"{user1} wins. Paper beats rock")
-                    wins.append(user2)
-                    loses.append(user1)
-
-                else:
-                    print("invalid move")
-            
-            # USER1 == ROCK
-            elif user1_answers[-1] == moves[2]: # rock beats scissors, rock lose paper
-                if user2_answers[-1] == moves[0]:
-                    print(f"{user1} wins. Rock beats Scissors")
-                    wins.append(user1)
-                    loses.append(user2)
-                
-                elif user2_answers[-1] == moves[1]:
-                    print(f"{user2} wins. Paper beats rock")
-                    wins.append(user2)
-                    loses.append(user1)
-
-                else:
-                    print("invalid move")
-            
-            play_again = input("Play again? (y/n): ")
-            if play_again.lower() != "y":
-                break
-
 def server_program():
     # get the hostname
     host = socket.gethostname()
@@ -90,24 +20,26 @@ def server_program():
     print(f"[LISTENING] Server is listening on {SERVER}")
     conn, address = server_socket.accept()  # accept new connection
 
+    global p2_name, p1_name, p2_move, p1_move
+
+    p1_name = input("Please input your name: ")
+    conn.send(p1_name.encode(FORMAT))
+
     p2_name = conn.recv(HEADER).decode(FORMAT)
     print(f"You are now chatting with {p2_name}")
 
-    P1_name = input("Please input your name: ")
-    conn.send(P1_name.encode(FORMAT))
-    
     connected = True
     while connected:
-        data = conn.recv(HEADER).decode(FORMAT)
-        if not data:
+        p2_move = conn.recv(HEADER).decode(FORMAT)
+        if not p2_move:
             # if data is not received break
             break
         
-        print(Fore.YELLOW + f"[{P1_name} - {socket.gethostbyname(host)}]")
-        print(Fore.CYAN, f"Received a message from {p2_name}: {data}")
-        data = input(' >>> ')
+        print(Fore.YELLOW + f"[{p1_name} - {socket.gethostbyname(host)}]")
+        print(Fore.CYAN, f"{p2_name} did {p2_move}")
+        p2_move = input(' >>> ')
         
-        conn.send(data.encode())  # send data to the client
+        conn.send(p2_move.encode())  # send data to the client
 
     conn.close()  # close the connection
 
