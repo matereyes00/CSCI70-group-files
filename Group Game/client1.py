b799a5cf918b0b25b3d22855c429596207183860
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2022-10-29 09:38:03 +0800

    Created a separate function to display game title. Tried creating a better way to restart the game when "play again" or invalid moves are invoked.

--- Group Game/client1.py
+++ Group Game/client1.py
@@ -4,81 +4,90 @@ from colorama import Fore, Back, Style
 from art import *
 init()
 
+def display_title():
+    game_title=text2art("RPS",font='block',chr_ignore=True)
+    print(game_title)
+
+def make_a_move():
+    print(f"Please choose 'Rock', 'Paper', or 'Scissors'")
+    player_move = input(">>> ")
+    return player_move
+
 def start_rps(p1Move, p2Move):
     moves = ["scissors", "paper", "rock"]
-    p1_name_answers = []
-    p2_name_answers = []
     wins = []
     loses = []
     while True:
-        p1_name_answers.append(p1Move)
-        p2_name_answers.append(p2Move)
-
-        if p1_name_answers[-1] == p2_name_answers[-1]: # TIE
+        p1Move = p1Move.lower()
+        p2Move = p2Move.lower()
+        if p1Move == p2Move: # TIE
             print("Tie!")
 
-        elif p1_name_answers[-1] != p2_name_answers[-1]: 
+        elif p1Move != p2Move: 
             # USER 1 == SCISSORS
-            if p1_name_answers[-1] == moves[0]: # scissors beat paper, scissors lose rock
-                if p2_name_answers[-1] == moves[1]:
+            if p1Move == moves[0]: # scissors beat paper, scissors lose rock
+                if p2Move == moves[1]:
                     print(f"{p1_name} wins. Scissors beats paper")
                     wins.append(p1_name)
                     loses.append(p2_name)
 
-                elif p2_name_answers[-1] == moves[2]:
+                elif p2Move == moves[2]:
                     print(f"{p2_name} wins. Rock beats scissors")
                     wins.append(p2_name)
                     loses.append(p1_name)
-                else:
-                    print("invalid move")
             
             # USER 1 == PAPER
-            elif p1_name_answers[-1] == moves[1]: # paper beats rock, paper lose scissors
-                if p2_name_answers[-1] == moves[0]:
+            elif p1Move == moves[1]: # paper beats rock, paper lose scissors
+                if p2Move == moves[0]:
                     print(f"{p2_name} wins. Scissors beats paper")
                     wins.append(p2_name)
                     loses.append(p1_name)
                 
-                elif p2_name_answers[-1] == moves[2]:
+                elif p2Move == moves[2]:
                     print(f"{p1_name} wins. Paper beats rock")
                     wins.append(p2_name)
                     loses.append(p1_name)
-
-                else:
-                    print("invalid move")
             
             # p1_name == ROCK
-            elif p1_name_answers[-1] == moves[2]: # rock beats scissors, rock lose paper
-                if p2_name_answers[-1] == moves[0]:
+            elif p1Move == moves[2]: # rock beats scissors, rock lose paper
+                if p2Move == moves[0]:
                     print(f"{p1_name} wins. Rock beats Scissors")
                     wins.append(p1_name)
                     loses.append(p2_name)
                 
-                elif p2_name_answers[-1] == moves[1]:
+                elif p2Move == moves[1]:
                     print(f"{p2_name} wins. Paper beats rock")
                     wins.append(p2_name)
                     loses.append(p1_name)
 
-                else:
-                    print("invalid move")
-            
             # not valid option
-            elif p1_name_answers[-1] not in moves:
-                print("Sorry, not a valid move.")
-                p1Move = input("Try again: ")
+            while p2Move not in moves:
+                print("Sorry, not a valid move. Try again:")
+                p2Move = make_a_move()
+                p2Move = p2Move.lower()
+                start_rps(p1Move, p2Move)
 
-            # not valid option
-            elif p2_name_answers[-1] not in moves:
-                print("Sorry, not a valid move.")
-                p2Move = input("Try again: ")
+            while p1Move not in moves:
+                print(f"Waiting for a valid answer from {p1_name}")
+                # start_rps(p1Move, p2Move)
 
 
         play_again = input("Play again? (y/n): ")
+        client_socket.send(play_again.encode(FORMAT))  # send message
+        play_again_resp = client_socket.recv(HEADER).decode(FORMAT)
         if play_again.lower() != "y":
-            break
+            # sending server that client doesnt want to play again
+            if play_again_resp == "n":
+                break
+            p2Move = make_a_move()
+            client_socket.send(p2Move.encode())  # send data to the client
+            p1Move = client_socket.recv(HEADER).decode(FORMAT) # receiving p1's move
+            start_rps(p1Move, p2Move)
 
 
 def client_program():
+
+    global host, port, HEADER, FORMAT, client_socket
     host = socket.gethostname()  # as both code is running on same pc
     port = 5000  # socket server port number
     HEADER = 64 
@@ -88,17 +97,14 @@ def client_program():
     client_socket.connect((host, port))  # connect to the server
 
     global p2_name, p1_name, p2_move, p1_move
-    game_title=text2art("RPS",font='block',chr_ignore=True)
-    print(game_title)
+    display_title()
 
     p2_name = input("Please input your name: ")
     client_socket.send(p2_name.encode(FORMAT))
     p1_name = client_socket.recv(HEADER).decode(FORMAT)
     print(f"You are player 2. You are now playing with {p1_name}")
-    print(f"Please choose 'Rock', 'Paper', or 'Scissors'")
-    p2_move = input(" >>> ")  # take input of p2move
     
-    
+    p2_move = make_a_move()  # take input of p2Move
     while p2_move.lower().strip() != 'exit()':
         client_socket.send(p2_move.encode(FORMAT))  # send message
         p1_move = client_socket.recv(HEADER).decode(FORMAT)  # receive response
--- Group Game/server1.py
+++ Group Game/server1.py
@@ -4,88 +4,100 @@ from colorama import Fore, Back, Style
 from art import *
 init()
 
+def display_title():
+    game_title=text2art("RPS",font='block',chr_ignore=True)
+    print(game_title)
+
+def make_a_move():
+    print(f"Please choose 'Rock', 'Paper', or 'Scissors'")
+    player_move = input(">>> ")
+    return player_move
 
 def start_rps(p1Move, p2Move):
     moves = ["scissors", "paper", "rock"]
-    p1_name_answers = []
-    p2_name_answers = []
+    # p1_name_answers = []
+    # p2_name_answers = []
     wins = []
     loses = []
     while True:
-        p1_name_answers.append(p1Move)
-        p2_name_answers.append(p2Move)
+        p1Move = p1Move.lower()
+        p2Move = p2Move.lower()
 
-        if p1_name_answers[-1] == p2_name_answers[-1]: # TIE
+        if p1Move == p2Move: # TIE
             print("Tie!")
 
-        elif p1_name_answers[-1] != p2_name_answers[-1]: 
+        elif p1Move != p2Move: 
             # USER 1 == SCISSORS
-            if p1_name_answers[-1] == moves[0]: # scissors beat paper, scissors lose rock
-                if p2_name_answers[-1] == moves[1]:
+            if p1Move == moves[0]: # scissors beat paper, scissors lose rock
+                if p2Move == moves[1]:
                     print(f"{p1_name} wins. Scissors beats paper")
                     wins.append(p1_name)
                     loses.append(p2_name)
 
-                elif p2_name_answers[-1] == moves[2]:
+                elif p2Move == moves[2]:
                     print(f"{p2_name} wins. Rock beats scissors")
                     wins.append(p2_name)
                     loses.append(p1_name)
-                else:
-                    print("invalid move")
             
             # USER 1 == PAPER
-            elif p1_name_answers[-1] == moves[1]: # paper beats rock, paper lose scissors
-                if p2_name_answers[-1] == moves[0]:
+            elif p1Move == moves[1]: # paper beats rock, paper lose scissors
+                if p2Move == moves[0]:
                     print(f"{p2_name} wins. Scissors beats paper")
                     wins.append(p2_name)
                     loses.append(p1_name)
                 
-                elif p2_name_answers[-1] == moves[2]:
+                elif p2Move == moves[2]:
                     print(f"{p1_name} wins. Paper beats rock")
                     wins.append(p2_name)
                     loses.append(p1_name)
-
-                else:
-                    print("invalid move")
             
             # p1_name == ROCK
-            elif p1_name_answers[-1] == moves[2]: # rock beats scissors, rock lose paper
-                if p2_name_answers[-1] == moves[0]:
+            elif p1Move == moves[2]: # rock beats scissors, rock lose paper
+                if p2Move == moves[0]:
                     print(f"{p1_name} wins. Rock beats Scissors")
                     wins.append(p1_name)
                     loses.append(p2_name)
                 
-                elif p2_name_answers[-1] == moves[1]:
+                elif p2Move == moves[1]:
                     print(f"{p2_name} wins. Paper beats rock")
                     wins.append(p2_name)
                     loses.append(p1_name)
-
-                else:
-                    print("invalid move")
             
             # not valid option
-            elif p1_name_answers[-1] not in moves:
-                print("Sorry, not a valid move.")
-                p1Move = input("Try again: ")
+            while p1Move not in moves:
+                print("Sorry, not a valid move. Try again:")
+                p1Move = make_a_move()
+                p1Move = p1Move.lower()
+                start_rps(p1Move, p2Move)
 
-            # not valid option
-            elif p2_name_answers[-1] not in moves:
-                print("Sorry, not a valid move.")
-                p2Move = input("Try again: ")
-
+            while p2Move not in moves:
+                print(f"Waiting for a valid answer from {p2_name}")
+                # start_rps(p1Move, p2Move)
 
         play_again = input("Play again? (y/n): ")
+        conn.send(play_again.encode(FORMAT))
+        play_again_resp = conn.recv(HEADER).decode(FORMAT)
         if play_again.lower() != "y":
-            break
+            # sending to client that the server doesn't want to play again
+            if play_again_resp == "n":
+                break
+            p1Move = make_a_move()
+            conn.send(p1Move.encode())  # send data to the client
+            p2Move = conn.recv(HEADER).decode(FORMAT) # receiving p2's move
+            start_rps(p1Move, p2Move)
 
 
 def server_program():
+
+    global host, port, HEADER, FORMAT, SERVER, conn
     # get the hostname
     host = socket.gethostname()
     port = 5000  # initiate port no above 1024
     HEADER = 64 
     FORMAT = 'utf-8'
     SERVER = socket.gethostbyname(socket.gethostname()) #get SERVER automatically for u
+    global p2_name, p1_name, p2_move, p1_move
+
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
     server_socket.bind((host, port))  # bind host address and port together
@@ -95,19 +107,14 @@ def server_program():
     print(f"[LISTENING] Server is listening on {SERVER}")
     conn, address = server_socket.accept()  # accept new connection
 
-    global p2_name, p1_name, p2_move, p1_move
-    game_title=text2art("RPS",font='block',chr_ignore=True)
-    print(game_title)
+    display_title()
 
     p1_name = input("Please input your name: ")
     conn.send(p1_name.encode(FORMAT))
-
     p2_name = conn.recv(HEADER).decode(FORMAT)
     print(f"You are player 1. You are now playing with {p2_name}")
 
-    print(f"Please choose 'Rock', 'Paper', or 'Scissors'")
-    p1_move = input(' >>> ')
-
+    p1_move = make_a_move()
     connected = True
     while connected:
         conn.send(p1_move.encode())  # send data to the client
